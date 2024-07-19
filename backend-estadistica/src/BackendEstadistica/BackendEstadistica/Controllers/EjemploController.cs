c2660efbfaa9d598f3962be06779a775010d5a6a
Sergio Melero <sergiomele97@gmail.com>
2024-07-19 10:31:26 +0200

    Lector de csv por API

---
+++ backend-estadistica/src/BackendEstadistica/BackendEstadistica/Controllers/EjemploController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using TuProyecto.Biblioteca;
+
+namespace BackendEstadistica.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EjemploController : ControllerBase
+    {
+        private readonly CSVReader _csvReader;
+
+        public EjemploController()
+        {
+            _csvReader = new CSVReader();
+        }
+
+        [HttpGet]
+        [Route("leer")]
+        public IActionResult LeerCSV([FromQuery] string filePath)
+        {
+            try
+            {
+                var datos = _csvReader.LeerArchivoCSV(filePath);
+                return Ok(datos);
+            }
+            catch (FileNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
+    }
+}
---
+++ backend-estadistica/src/BackendEstadistica/ClassLibraryEjemplo/CSVReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TuProyecto.Biblioteca
+{
+    public class CSVReader
+    {
+        public IEnumerable<string[]> LeerArchivoCSV(string filePath)
+        {
+            var resultados = new List<string[]>();
+
+            try
+            {
+                // Verificar si el archivo existe
+                if (!File.Exists(filePath))
+                {
+                    throw new FileNotFoundException("El archivo no existe.");
+                }
+
+                // Leer el archivo CSV
+                using (var reader = new StreamReader(filePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+
+                        // Añadir los valores leídos a la lista de resultados
+                        resultados.Add(values);
+                    }
+                }
+            }
+            catch (IOException ioEx)
+            {
+                throw new Exception($"Error de E/S: {ioEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: {ex.Message}");
+            }
+
+            return resultados;
+        }
+    }
+}
