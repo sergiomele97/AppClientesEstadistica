e104f1f4b00fc40dfd5ff1b7238e2fe4312d1bcb
Sergio Melero <sergiomele97@gmail.com>
2024-07-22 14:27:34 +0200

    Entity framework

---
+++ backend-estadistica/src/BackendEstadistica/Contexto/ContextoBBDD.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackendEstadistica.Contexto
+{
+    public class ContextoBBDD : DbContext
+    {
+        public static UsuariosRepositorioMemoria Instancia { get; } = new UsuariosRepositorioMemoria();
+
+        public ContextoBBDD()
+        {
+
+        }
+
+    }
+}
---
+++ backend-estadistica/src/BackendEstadistica/Entidades/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace BackendEstadistica.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Correo { get; set; }
+        public string? Contraseña { get; set; }
+
+    }
+}
