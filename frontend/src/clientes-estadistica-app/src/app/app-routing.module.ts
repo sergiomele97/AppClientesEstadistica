b6fd565243b31fe92094a682b030d9f04cc93a97
Sergio Melero <sergiomele97@gmail.com>
2024-07-22 14:05:26 +0200

    Inicio proyecto angular

---
+++ frontend/src/clientes-estadistica-app/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
+
+const routes: Routes = [
+  {path: "registro", component: RegistroComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/clientes-estadistica-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegistroComponent } from './registro/registro.component';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+      RegistroComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
