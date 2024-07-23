ed6954675272de6ccb57f9350de598b9c7bf39f3
Sergio Melero <sergiomele97@gmail.com>
2024-07-23 10:00:27 +0200

    Se implementa error 404

--- backend-estadistica/src/BackendEstadistica/Controllers/UsuariosController.cs
+++ backend-estadistica/src/BackendEstadistica/Controllers/UsuariosController.cs
@@ -1,4 +1,6 @@
-﻿namespace BackendEstadistica.Controllers;
+﻿using Microsoft.EntityFrameworkCore.Internal;
+
+namespace BackendEstadistica.Controllers;
 
 [Route("api/usuarios")]
 [ApiController]
@@ -13,6 +15,15 @@ public class UsuariosController : ControllerBase
     [HttpGet("{id}")]
     public IActionResult Get(int id)
     {
-        return Ok(UsuariosRepositorioMemoria.Instancia.Usuarios.FirstOrDefault(u => u.Id == id));
+        var usuario = UsuariosRepositorioMemoria.Instancia.Usuarios.FirstOrDefault(u => u.Id == id);
+        if (usuario == null)
+        {
+            // NotFound() -> Devuelve un objeto NotFoundResult que produce una respuesta HTTP 404.
+            return NotFound();
+        }
+
+        // El método FirstOrDefault() devuelve el primer elemento de una secuencia o uno predeterminado
+        return Ok(usuario);
     }
 }
+
--- backend-estadistica/src/BackendEstadistica/Entidades/Usuario.cs
+++ backend-estadistica/src/BackendEstadistica/Entidades/Usuario.cs
@@ -1,8 +1,13 @@
-﻿namespace BackendEstadistica.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendEstadistica.Entidades
 {
     public class Usuario
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string? Nombre { get; set; }
         public string? Correo { get; set; }
         public string? Contraseña { get; set; }
