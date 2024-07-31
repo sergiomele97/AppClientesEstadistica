fef812bfbc88406aa783f0c542d6fada18086888
Sergio Melero <sergiomele97@gmail.com>
2024-07-31 10:30:43 +0200

    Arreglo registro

--- frontend/src/clientes-estadistica-app/src/app/app.module.ts
+++ frontend/src/clientes-estadistica-app/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './registro/registro.component';
-
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
-
 import { UserService } from './servicios/user.service';
 import { FormsModule } from '@angular/forms';
 import { ClientesComponent } from './clientes/clientes.component';
@@ -15,14 +12,13 @@ import { UsuariosComponent } from './usuarios/usuarios.component';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [				
+  declarations: [					
     AppComponent,
-      RegistroComponent,
-      LoginComponent,
-      RegistroComponent,
-      ClientesComponent,
-      UsuariosComponent,
-      MenuComponent
+    RegistroComponent,
+    LoginComponent,
+    ClientesComponent,
+    UsuariosComponent,
+    MenuComponent,
    ],
   imports: [
     BrowserModule,
