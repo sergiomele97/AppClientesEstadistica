6253534254ba65b8579005f01553de18e56154ae
rudakar <rubenberuete.1999@gmail.com>
2024-07-31 13:30:34 +0200

    Modificado servicio y clase de autenticacion. Realizadas pruebas de inicio de sesion con un usuario existente en la BBDD

--- frontend/src/clientes-estadistica-app/src/app/servicios/user.service.ts
+++ frontend/src/clientes-estadistica-app/src/app/servicios/user.service.ts
@@ -3,28 +3,26 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Usuario } from '../clases/usuario';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class UserService {
 
-    private url = 'https://localhost:7144/api/usuarios';    // Cambia esta URL por la de tu backend
+  private url = 'https://localhost:44339/api/Account'; // Cambia esta URL por la de tu backend
 
-// Constructor que instancia un servicio http
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-getUsuarios(): Observable<Usuario[]> {
+  getUsuarios(): Observable<Usuario[]> {
     return this.http.get<Usuario[]>(this.url);
-}
-
-autenticarUsuario(email: string, password: string): Observable<Usuario[]> {     // pendiente (enviaremos solo 1 usuario y se comprobara en back)
-    return this.http.get<Usuario[]>(`${this.url}?email=${email}&password=${password}`);
-}
-
-// Método con parámetro tipo usuario
-registrarUsuario(usuario: Usuario): Observable<Usuario> {
-    console.log(usuario); // Asegúrate de que los campos estén presentes y correctos DEBUG
-    return this.http.post<Usuario>(`${this.url}/crearUsuario`, usuario);
   }
 
-}
+  autenticarUsuario(email: string, password: string, remember: boolean): Observable<any> {
+    remember = false;
+    return this.http.post<any>(`${this.url}/login`, { email, password, remember });
+  }
 
-  
+  registrarUsuario(usuario: Usuario): Observable<Usuario> {
+    console.log(usuario); // Asegúrate de que los campos estén presentes y correctos DEBUG
+    return this.http.post<Usuario>(`${this.url}/register`, usuario);
+  }
+}
