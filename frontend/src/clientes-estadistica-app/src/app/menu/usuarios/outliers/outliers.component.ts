838673e16e1b922e0dad8a65ede8eb3101b7e0fa
Carlos <carlos.ibanez1998@gmail.com>
2024-07-31 10:54:09 +0200

    añadida pagina outliers

---
+++ frontend/src/clientes-estadistica-app/src/app/menu/usuarios/outliers/outliers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-outliers',
+  templateUrl: './outliers.component.html',
+  styleUrls: ['./outliers.component.css']
+})
+export class OutliersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
