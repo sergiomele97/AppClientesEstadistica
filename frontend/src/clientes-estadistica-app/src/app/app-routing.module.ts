e349333c73866834ffe650254ae377c3e5e7e848
Carlos <carlos.ibanez1998@gmail.com>
2024-07-31 10:57:59 +0200

    routing subpaginas usuario

--- frontend/src/clientes-estadistica-app/src/app/app-routing.module.ts
+++ frontend/src/clientes-estadistica-app/src/app/app-routing.module.ts
@@ -6,6 +6,12 @@ import { MenuComponent } from './menu/menu.component';
 import { ClientesComponent } from './menu/clientes/clientes.component';
 import { UsuariosComponent } from './menu/usuarios/usuarios.component';
 import { OutliersComponent } from './menu/usuarios/outliers/outliers.component';
+import { SpaghettiComponent } from './menu/usuarios/spaghetti/spaghetti.component';
+import { GraphComponent } from './menu/usuarios/graph/graph.component';
+import { MapaComponent } from './menu/usuarios/mapa/mapa.component';
+import { ClusterComponent } from './menu/usuarios/cluster/cluster.component';
+import { VolumetriaComponent } from './menu/usuarios/volumetria/volumetria.component';
+import { TableComponent } from './menu/usuarios/table/table.component';
 const routes: Routes = [
   { path: "login", component: LoginComponent},
   { path: "", redirectTo: "/login", pathMatch: "full" },
@@ -16,7 +22,13 @@ const routes: Routes = [
       { path: "clientes", component: ClientesComponent},
       { path: "usuarios", component: UsuariosComponent,
         children: [
-          {path: "outliers", component: OutliersComponent}
+          {path: "outliers", component: OutliersComponent},
+          {path: "volumetria", component: VolumetriaComponent},
+          {path: "cluster", component: ClusterComponent},
+          {path: "mapa", component: MapaComponent},
+          {path: "graph", component: GraphComponent},
+          {path: "spaghetti", component: SpaghettiComponent},
+          {path: "tabla", component: TableComponent},
         ]
       }
     ]
