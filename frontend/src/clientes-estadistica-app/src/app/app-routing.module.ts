77a8ed9999da345215b6036f34e4f01b8c830cbd
Carlos <carlos.ibanez1998@gmail.com>
2024-07-31 10:53:33 +0200

    reconfiguracion routing

--- frontend/src/clientes-estadistica-app/src/app/app-routing.module.ts
+++ frontend/src/clientes-estadistica-app/src/app/app-routing.module.ts
@@ -5,15 +5,23 @@ import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { ClientesComponent } from './menu/clientes/clientes.component';
 import { UsuariosComponent } from './menu/usuarios/usuarios.component';
-
+import { OutliersComponent } from './menu/usuarios/outliers/outliers.component';
 const routes: Routes = [
   { path: "login", component: LoginComponent},
   { path: "", redirectTo: "/login", pathMatch: "full" },
   { path: "*", redirectTo: "/login", pathMatch: "full" },
   { path: "registro", component: RegistroComponent },
-  { path: "menu", component: MenuComponent},
-  { path: "clientes", component: ClientesComponent},
-  { path: "usuarios", component: UsuariosComponent}
+  { path: "menu", component: MenuComponent,
+    children: [
+      { path: "clientes", component: ClientesComponent},
+      { path: "usuarios", component: UsuariosComponent,
+        children: [
+          {path: "outliers", component: OutliersComponent}
+        ]
+      }
+    ]
+  },
+
 ];
 
 @NgModule({
