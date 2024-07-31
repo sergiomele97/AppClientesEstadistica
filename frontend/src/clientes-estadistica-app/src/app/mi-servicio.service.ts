f9ff8057671dc721eef0557cad63bc957f15c6ac
Sergio Melero <sergiomele97@gmail.com>
2024-07-31 09:49:01 +0200

    cambio servicio

--- frontend/src/clientes-estadistica-app/src/app/mi-servicio.service.ts
+++ frontend/src/clientes-estadistica-app/src/app/mi-servicio.service.ts
@@ -2,17 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class MiServicioService {
 
-  private apiUrl = 'https://localhost:7144/api/usuarios'; // URL de tu backend
+  private apiUrl = 'https://localhost:7144/api/usuarios'; // Cambia esta URL por la de tu backend
 
   constructor(private http: HttpClient) { }
+
+  registrarUsuario(usuario: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/crearUsuario`, usuario);
+  }
 
   getUsuarios(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
   }
-  
 }
