2355a2acb80c37fea18835dc813b22276d4cd164
Carlos <carlos.ibanez1998@gmail.com>
2024-07-31 10:54:37 +0200

    añadida pagina volumetria

---
+++ frontend/src/clientes-estadistica-app/src/app/menu/usuarios/volumetria/volumetria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-volumetria',
+  templateUrl: './volumetria.component.html',
+  styleUrls: ['./volumetria.component.css']
+})
+export class VolumetriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
