37132a181e352444251667574881a0c3983dfe23
Carlos <carlos.ibanez1998@gmail.com>
2024-08-01 10:25:18 +0200

    volumetry clean

--- frontend/src/clientes-estadistica-app/src/app/menu/usuarios/volumetry/volumetry.module.ts
+++ frontend/src/clientes-estadistica-app/src/app/menu/usuarios/volumetry/volumetry.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgApexchartsModule } from 'ng-apexcharts';
 
 @NgModule({
   imports: [
-    CommonModule,
-    NgApexchartsModule
+    CommonModule
   ],
 })
 export class VolumetryModule { }
