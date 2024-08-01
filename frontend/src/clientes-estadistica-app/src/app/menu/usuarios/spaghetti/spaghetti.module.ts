7f7551251c63606a1c657775f99636b8751351c7
Carlos <carlos.ibanez1998@gmail.com>
2024-08-01 13:18:21 +0200

    spaghetti graph

--- frontend/src/clientes-estadistica-app/src/app/menu/usuarios/spaghetti/spaghetti.module.ts
+++ frontend/src/clientes-estadistica-app/src/app/menu/usuarios/spaghetti/spaghetti.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SpaghettiComponent } from './spaghetti.component';
 
 @NgModule({
   imports: [
     CommonModule
-  ],
-  declarations: [SpaghettiComponent]
+  ]
 })
 export class SpaghettiModule { }
