6f93de361cdd07cdfd721bbe95b87a3959f99b32
Carlos <carlos.ibanez1998@gmail.com>
2024-08-01 13:20:50 +0200

    outliersbarcomponent etror

--- frontend/src/clientes-estadistica-app/src/app/menu/usuarios/outliers/outliers.module.ts
+++ frontend/src/clientes-estadistica-app/src/app/menu/usuarios/outliers/outliers.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OutliersComponent } from './outliers.component';
 
 @NgModule({
   imports: [
     CommonModule
-  ],
-  declarations: [OutliersComponent]
+  ]
 })
 export class OutliersModule { }
