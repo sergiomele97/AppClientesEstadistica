4efcaaa2ed93cd35b2211f26bf6e369796c848c1
Carlos <carlos.ibanez1998@gmail.com>
2024-08-02 12:49:53 +0200

    clusters-table

---
+++ frontend/src/clientes-estadistica-app/src/app/menu/usuarios/clusters/clusters-table/clusters-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clusters-table',
+  templateUrl: './clusters-table.component.html',
+  styleUrls: ['./clusters-table.component.css']
+})
+export class ClustersTableComponent {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
