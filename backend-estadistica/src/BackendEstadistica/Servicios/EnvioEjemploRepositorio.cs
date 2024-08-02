1e2ca60da7c77972dfa46025a1b11bcb64a465fc
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-08-02 13:55:30 +0200

    Creamos clase EnvioEjemploFaker, para implementar Bogus

--- backend-estadistica/src/BackendEstadistica/Servicios/EnvioEjemploRepositorio.cs
+++ backend-estadistica/src/BackendEstadistica/Servicios/EnvioEjemploRepositorio.cs
@@ -1,9 +1,11 @@
-﻿namespace BackendEstadistica.Servicios
+﻿
+namespace BackendEstadistica.Servicios
 {
     public class EnvioEjemploRepositorio : IEnvioEjemploRepositorio
     {
-
-
-
+        public Task<EnvioEjemplo> GetEnvio(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
