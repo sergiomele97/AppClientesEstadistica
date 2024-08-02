f73624d0b012b93bceaf8a22e534046266f96001
Carlos <carlos.ibanez1998@gmail.com>
2024-08-02 13:15:32 +0200

    outliers detail graph

---
+++ frontend/src/clientes-estadistica-app/src/app/menu/usuarios/outliers/outliers-detail/outliers-detail-graph/outliers-detail-graph.component.ts
@@ -0,0 +1,143 @@
+import { Component, ViewChild } from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexYAxis,
+  ApexTitleSubtitle,
+  ApexXAxis,
+  ApexFill
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  fill: ApexFill;
+  title: ApexTitleSubtitle;
+};
+
+@Component({
+  selector: 'app-outliers-detail-graph',
+  templateUrl: './outliers-detail-graph.component.html',
+  styleUrls: ['./outliers-detail-graph.component.css']
+})
+
+export class OutliersDetailGraphComponent {
+
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Inflation",
+          data: [10,12,5,2.3,3.4,1296.32,5]
+        }
+      ],
+      chart: {
+        height: 0,
+        type: "bar"
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            position: "top" // top, center, bottom
+          }
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function(val) {
+          return val + "€";
+        },
+        offsetY: 0,
+        style: {
+          fontSize: "12px",
+          colors: ["#304758"]
+        }
+      },
+
+      xaxis: {
+        categories: [
+          "Lunes",
+          "Martes",
+          "Miercoles",
+          "Jueves",
+          "Viernes",
+          "Sabado",
+          "Domingo",
+        ],
+        position: "bottom",
+        labels: {
+          offsetY: 0
+        },
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        crosshairs: {
+          fill: {
+            type: "gradient",
+            gradient: {
+              colorFrom: "#D8E3F0",
+              colorTo: "#BED1E6",
+              stops: [0, 100],
+              opacityFrom: 0.4,
+              opacityTo: 0.5
+            }
+          }
+        },
+        tooltip: {
+          enabled: true,
+          offsetY: 0
+        }
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shade: "light",
+          type: "horizontal",
+          shadeIntensity: 0.25,
+          gradientToColors: undefined,
+          inverseColors: true,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [50, 0, 100, 100]
+        }
+      },
+      yaxis: {
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        labels: {
+          show: false,
+          formatter: function(val) {
+            return val + "%";
+          }
+        }
+      },
+      title: {
+        text: "Tabla de envios de dinero",
+        floating: false,
+        offsetY: 0,
+        align: "center",
+        style: {
+          color: "#444"
+        }
+      }
+    };
+  }
+
+}
