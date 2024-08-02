1909582bcde1c69dca1d705a0e1200359c2eab6b
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-08-02 14:24:59 +0200

    Añadimos funcionalidad al EnvioEjemplo repo

--- backend-estadistica/src/BackendEstadistica/Servicios/EnvioEjemploRepositorio.cs
+++ backend-estadistica/src/BackendEstadistica/Servicios/EnvioEjemploRepositorio.cs
@@ -1,11 +1,27 @@
 ﻿
+using BackendEstadistica.Contexto;
+
 namespace BackendEstadistica.Servicios
 {
     public class EnvioEjemploRepositorio : IEnvioEjemploRepositorio
     {
-        public Task<EnvioEjemplo> GetEnvio(int id)
+
+        private readonly ContextoBBDD contextoBBDD;
+
+        public EnvioEjemploRepositorio(ContextoBBDD contextoBBDD)
         {
-            throw new NotImplementedException();
+            this.contextoBBDD = contextoBBDD;
+        }
+
+        public List<EnvioEjemplo> GetEnvio()
+        {
+            var envioFake = new EnvioEjemploFaker().Generate();
+
+            contextoBBDD.EnvioEjemplo.Add(envioFake);
+            contextoBBDD.SaveChanges();
+
+           return contextoBBDD.EnvioEjemplo.ToList();
+            
         }
     }
 }
