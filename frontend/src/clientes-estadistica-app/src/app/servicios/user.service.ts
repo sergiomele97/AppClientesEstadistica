f02f431c7f3db3c610d4192fd84faf284c1682e6
Sergio Melero <sergiomele97@gmail.com>
2024-08-05 15:21:03 +0200

    Cambio nombre url estadistica

--- frontend/src/clientes-estadistica-app/src/app/servicios/user.service.ts
+++ frontend/src/clientes-estadistica-app/src/app/servicios/user.service.ts
@@ -10,27 +10,29 @@ import { environment } from '../../environments/environment';
 })
 export class UserService {
 
-  private url = environment.apiUrl; 
+  // IMPORTANTE:
+  //    1 URL ESTADISTICA Y OTRA PARA CLIENTES
+  private url_estadistica = environment.apiUrl; 
 
   constructor(private http: HttpClient) { }
 
   getUsuarios(): Observable<Usuario[]> {
-    return this.http.get<Usuario[]>(`${this.url}Account/users`);
+    return this.http.get<Usuario[]>(`${this.url_estadistica}Account/users`);
   }
   
   autenticarUsuario(email: string, password: string, remember: boolean): Observable<any> {
     remember = false;
-    return this.http.post<any>(`${this.url}Account/login`, { email, password, remember });
+    return this.http.post<any>(`${this.url_estadistica}Account/login`, { email, password, remember });
   }
 
   registrarUsuario(usuario: any): Observable<any> {
     console.log(usuario); // Asegúrate de que los campos estén presentes y correctos DEBUG
-    return this.http.post<any>(`${this.url}Account/register`, usuario);
+    return this.http.post<any>(`${this.url_estadistica}Account/register`, usuario);
   }
 
   obtenerPaisIdPorNombre(nombre: string): Observable<{ id: number }> {
     // Construir la URL con el nombre del país
-    const url = `${this.url}Paises/nombre/${nombre}`;
+    const url = `${this.url_estadistica}Paises/nombre/${nombre}`;
     
     // Hacer la solicitud GET
     return this.http.get<{ id: number }>(url);
