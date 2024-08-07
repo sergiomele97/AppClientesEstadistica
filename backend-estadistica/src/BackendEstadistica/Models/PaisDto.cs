2afc7f6183ada8fea57f7486c85adf8fd748729a
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-08-07 10:46:32 +0200

    Dto de pais

---
+++ backend-estadistica/src/BackendEstadistica/Models/PaisDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendEstadistica.Models;
+
+public class PaisDto
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+    public string? Divisa { get; set; }
+
+}
