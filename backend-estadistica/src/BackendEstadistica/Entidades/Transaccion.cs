0110133ffdae2dc8d176a523da9265233f024bd7
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-08-07 10:53:11 +0200

    Creo entidad transaccion

---
+++ backend-estadistica/src/BackendEstadistica/Entidades/Transaccion.cs
@@ -0,0 +1,16 @@
+﻿namespace BackendEstadistica.Entidades;
+
+public class Transaccion
+{
+    [Key]
+    public int Id { get; set; }
+    public double? importeRecibido { get; set; }
+    public double? importeEnviado { get; set; }
+    public DateTime? Fecha { get; set; }
+    public int idOrigen { get; set; }
+    public int idDestino { get; set; }
+
+
+
+
+}
