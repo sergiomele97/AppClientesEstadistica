eddc522a95c7a0d8f96f8621edd94aec6146ea6c
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-08-07 10:29:49 +0200

    Creo entidad clientes

---
+++ backend-estadistica/src/BackendEstadistica/Entidades/Clientes.cs
@@ -0,0 +1,21 @@
+﻿namespace BackendEstadistica.Entidades
+{
+    public class Clientes
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Contraseña { get; set; }
+        public string? Correo { get; set; }
+        public int? Telefono { get; set; }
+        public int? Edad {  get; set; }
+        public string? Sexo { get; set; }
+        public string? Trabajo { get; set; }
+
+
+
+
+
+    }
+}
--- backend-estadistica/src/BackendEstadistica/Entidades/Usuario.cs
+++ backend-estadistica/src/BackendEstadistica/Entidades/Usuario.cs
@@ -14,7 +14,7 @@ namespace BackendEstadistica.Entidades
         public int Id { get; set; }
         [Required]
         [StringLength(50)]
-        public string? Nombre { get; set; }
+        public string? Rol { get; set; }
         public string? Correo { get; set; }
         public string? Contraseña { get; set; }
 
