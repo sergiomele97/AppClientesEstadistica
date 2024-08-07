18c84044973ba9968d7067035e05e427fcf2abf1
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-08-07 13:56:22 +0200

    Creo clase Faker de cliente y cambio variable telefono

--- backend-estadistica/src/BackendEstadistica/Entidades/Cliente.cs
+++ backend-estadistica/src/BackendEstadistica/Entidades/Cliente.cs
@@ -8,7 +8,7 @@ public class Cliente
     public string? Nombre { get; set; }
     public string? Contraseña { get; set; }
     public string? Correo { get; set; }
-    public int? Telefono { get; set; }
+    public string? Telefono { get; set; }
     public int? Edad { get; set; }
     public string? Sexo { get; set; }
     public string? Trabajo { get; set; }
---
+++ backend-estadistica/src/BackendEstadistica/Faker/ClienteFaker.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+
+namespace BackendEstadistica.Faker;
+
+public class ClienteFaker : Faker<Cliente>
+{
+
+    public ClienteFaker()
+    {
+        RuleFor(c => c.Nombre, f => f.Name.FullName())
+            .RuleFor(c => c.Contraseña, f => f.Internet.Password())
+            .RuleFor(c => c.Correo, f => f.Internet.Email())
+            .RuleFor(c => c.Telefono, f => f.Phone.PhoneNumber("##########"))   
+            .RuleFor(c => c.Edad, f => f.Random.Int(18, 80))
+            .RuleFor(c => c.Sexo, f => f.PickRandom(new[] { "Masculino", "Femenino" }))
+            .RuleFor(c => c.Trabajo, f => f.Name.JobTitle());
+            //.RuleFor(c => c.Conversionos, f => new ConversionFaker().Generate(f.Random.Int(1, 5)));
+    }
+
+}
