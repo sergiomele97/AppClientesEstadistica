068f440cad36f06be6e411d641791f763e2531a9
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-08-07 10:41:16 +0200

    Creo entidad pais

---
+++ backend-estadistica/src/BackendEstadistica/Entidades/Pais.cs
@@ -0,0 +1,10 @@
+﻿namespace BackendEstadistica.Entidades;
+
+public class Pais
+{
+    [Key]
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+    public string? Divisa { get; set; }
+
+}
