b29b3d05459efdea3608484cf025d0266b906178
Carlos <carlos.ibanez1998@gmail.com>
2024-08-07 11:00:57 +0200

    outliers envios

--- frontend/src/clientes-estadistica-app/src/app/menu/usuarios/outliers/outliers-detail/outliers-detail-table/outliers-detail-table.component.ts
+++ frontend/src/clientes-estadistica-app/src/app/menu/usuarios/outliers/outliers-detail/outliers-detail-table/outliers-detail-table.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/servicios/user.service';
+import { Subscription } from 'rxjs';
+import { IEnvio } from 'src/app/interfaces/envios';
+import { EnviosService } from 'src/app/servicios/envios.service';
 
 @Component({
   selector: 'app-outliers-detail-table',
@@ -7,22 +9,22 @@ import { UserService } from 'src/app/servicios/user.service';
   styleUrls: ['./outliers-detail-table.component.css']
 })
 export class OutliersDetailTableComponent implements OnInit {
+  
+  constructor(private enviosService: EnviosService) {}
 
-
-  envios: any[];
-
-  constructor( private userService: UserService ) { }
-
-  ngOnInit() {
-    this.getEnvios();
-  }
-
-  getEnvios() {
-
-    this.userService.getEnvios().subscribe( datos=> {
-      this.envios = datos;
+  ngOnInit(): void {
+    this.subscription = this.enviosService.getEnvios().subscribe({
+      next: (envios) => {
+        this.envios = this.enviosService.formatEnvios(envios);
+      },
     });
-
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  subscription!: Subscription;
+  envios: IEnvio[] = [];
+  enviosFilter: IEnvio[] = [];
 }
