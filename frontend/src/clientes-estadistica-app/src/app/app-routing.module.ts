384d4bcb8b48b772afe13a9fb332bf8ebb523947
Carlos <carlos.ibanez1998@gmail.com>
2024-08-08 12:46:30 +0200

    table redirects cliente

--- frontend/src/clientes-estadistica-app/src/app/app-routing.module.ts
+++ frontend/src/clientes-estadistica-app/src/app/app-routing.module.ts
@@ -36,6 +36,7 @@ const routes: Routes = [
       { path: 'spaghetti', component: SpaghettiComponent },
       { path: 'table', component: TableComponent },
       { path: 'clientes', component: ClientesComponent },
+      { path: 'clientes/:id', component: ClientesComponent },
       { path: 'divisas', component: DivisasComponent },
     ],
   },
