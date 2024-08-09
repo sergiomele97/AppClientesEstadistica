0feaa79ef10001e82a5aed18043c2062c54f21f4
Almudena Iparraguirre <almudenaiparraguirre@gmail.com>
2024-08-09 13:14:42 +0200

    Bogus de países ya funcionando

--- backend-clientes/ApiBasesDeDatosSolucion/ApiBasesDeDatosProyecto/Controllers/PaisesController.cs
+++ backend-clientes/ApiBasesDeDatosSolucion/ApiBasesDeDatosProyecto/Controllers/PaisesController.cs
@@ -1,156 +1,153 @@
-﻿
-using Microsoft.AspNetCore.Authorization;
+﻿namespace ApiBasesDeDatosProyecto.Controllers;
 
-namespace ApiBasesDeDatosProyecto.Controllers
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class PaisesController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [Authorize]
-    public class PaisesController : ControllerBase
+    private readonly IPaisRepository _paisRepository;
+    private readonly IMapper _mapper;
+    private readonly ILogger<PaisesController> _logger;
+
+    public PaisesController(IPaisRepository paisRepository, IMapper mapper, ILogger<PaisesController> logger)
     {
-        private readonly IPaisRepository _paisRepository;
-        private readonly IMapper _mapper;
-        private readonly ILogger<PaisesController> _logger;
+        _paisRepository = paisRepository;
+        _mapper = mapper;
+        _logger = logger;
+    }
 
-        public PaisesController(IPaisRepository paisRepository, IMapper mapper, ILogger<PaisesController> logger)
+    // GET: api/paises
+    [HttpGet]
+    public async Task<ActionResult<List<PaisDto>>> Get()
+    {
+        _logger.LogInformation("Obteniendo todos los países.");
+        List<Pais> lista = await _paisRepository.ObtenerTodos();
+        _logger.LogInformation($"Se obtuvieron {lista.Count} países.");
+        return Ok(_mapper.Map<List<PaisDto>>(lista));
+    }
+
+    // GET api/paises/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<PaisDto>> Get(int id)
+    {
+        _logger.LogInformation($"Obteniendo país con id {id}.");
+        var pais = await _paisRepository.ObtenerPorId(id);
+        if (pais == null)
         {
-            _paisRepository = paisRepository;
-            _mapper = mapper;
-            _logger = logger;
+            _logger.LogWarning($"No se ha encontrado el país con id {id}.");
+            return NotFound();
         }
 
-        // GET: api/paises
-        [HttpGet]
-        public async Task<ActionResult<List<PaisDto>>> Get()
+        _logger.LogInformation($"Obtenido país con id {id}.");
+        return Ok(_mapper.Map<PaisDto>(pais));
+    }
+
+    [HttpGet("nombre/{nombre}")]
+    public async Task<IActionResult> ObtenerPorNombre(string nombre)
+    {
+        var pais = await _paisRepository.ObtenerPorNombre(nombre);
+        if (pais == null)
         {
-            _logger.LogInformation("Obteniendo todos los países.");
-            List<Pais> lista = await _paisRepository.ObtenerTodos();
-            _logger.LogInformation($"Se obtuvieron {lista.Count} países.");
-            return Ok(_mapper.Map<List<PaisDto>>(lista));
+            return NotFound();
+        }
+        return Ok(new { Id = pais.Id });
+    }
+
+    [HttpGet("GenerarPaisesFake")]
+    [AllowAnonymous]
+    public List<Pais> GenerarPaisesFake()
+    {
+        var paisFaker = new PaisFaker();
+        var paises = paisFaker.Generate(50);
+
+        return paises;
+    }
+
+    // POST api/paises
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] PaisDto paisDto)
+    {
+        _logger.LogInformation("Creando un nuevo país.");
+        //string cadena = new string('A', 5000);
+        //paisDto.Nombre += cadena;
+        if (paisDto == null)
+        {
+            _logger.LogWarning("El objeto PaisDto recibido es nulo.");
+            return BadRequest("El objeto PaisDto no puede ser nulo.");
         }
 
-        // GET api/paises/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<PaisDto>> Get(int id)
+        // Validar el modelo
+        if (!ModelState.IsValid)
         {
-            _logger.LogInformation($"Obteniendo país con id {id}.");
-            var pais = await _paisRepository.ObtenerPorId(id);
-            if (pais == null)
-            {
-                _logger.LogWarning($"No se ha encontrado el país con id {id}.");
-                return NotFound();
-            }
-
-            _logger.LogInformation($"Obtenido país con id {id}.");
-            return Ok(_mapper.Map<PaisDto>(pais));
+            _logger.LogWarning("El modelo PaisDto no es válido.");
+            return BadRequest(ModelState);
         }
 
-        [HttpGet("nombre/{nombre}")]
-        public async Task<IActionResult> ObtenerPorNombre(string nombre)
+        var pais = _mapper.Map<Pais>(paisDto);
+        _paisRepository.Agregar(pais);
+
+        if (await _paisRepository.GuardarCambios())
         {
-            var pais = await _paisRepository.ObtenerPorNombre(nombre);
-            if (pais == null)
-            {
-                return NotFound();
-            }
-            return Ok(new { Id = pais.Id });
+            _logger.LogInformation($"País con ID {pais.Id} creado correctamente.");
+            return CreatedAtAction(nameof(Get), new { id = pais.Id }, paisDto);
         }
 
-       /* [HttpGet("GenerarPaisesFake")]
-        public List<Pais> GenerarPaisesFake()
+        _logger.LogError("No se pudo agregar el país.");
+        return BadRequest("No se pudo agregar el país.");
+    }
+
+    // PUT api/paises/5
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Put(int id, [FromBody] PaisDto paisDto)
+    {
+        _logger.LogInformation($"Actualizando país con ID {id}.");
+        if (id != paisDto.PaisId)
         {
-            var paisFaker = new PaisFaker();
-            var paises = paisFaker.Generate(50);
-
-            return paises;
-        }*/
-
-        // POST api/paises
-        [HttpPost]
-        public async Task<ActionResult> Post([FromBody] PaisDto paisDto)
-        {
-            _logger.LogInformation("Creando un nuevo país.");
-            //string cadena = new string('A', 5000);
-            //paisDto.Nombre += cadena;
-            if (paisDto == null)
-            {
-                _logger.LogWarning("El objeto PaisDto recibido es nulo.");
-                return BadRequest("El objeto PaisDto no puede ser nulo.");
-            }
-
-            // Validar el modelo
-            if (!ModelState.IsValid)
-            {
-                _logger.LogWarning("El modelo PaisDto no es válido.");
-                return BadRequest(ModelState);
-            }
-
-            var pais = _mapper.Map<Pais>(paisDto);
-            _paisRepository.Agregar(pais);
-
-            if (await _paisRepository.GuardarCambios())
-            {
-                _logger.LogInformation($"País con ID {pais.Id} creado correctamente.");
-                return CreatedAtAction(nameof(Get), new { id = pais.Id }, paisDto);
-            }
-
-            _logger.LogError("No se pudo agregar el país.");
-            return BadRequest("No se pudo agregar el país.");
+            _logger.LogWarning($"ID del país en la solicitud ({paisDto.PaisId}) no coincide con el ID de la URL ({id}).");
+            return BadRequest("ID del país no coincide.");
         }
 
-        // PUT api/paises/5
-        [HttpPut("{id}")]
-        public async Task<ActionResult> Put(int id, [FromBody] PaisDto paisDto)
+        // Validar el modelo
+        if (!ModelState.IsValid)
         {
-            _logger.LogInformation($"Actualizando país con ID {id}.");
-            if (id != paisDto.PaisId)
-            {
-                _logger.LogWarning($"ID del país en la solicitud ({paisDto.PaisId}) no coincide con el ID de la URL ({id}).");
-                return BadRequest("ID del país no coincide.");
-            }
-
-            // Validar el modelo
-            if (!ModelState.IsValid)
-            {
-                _logger.LogWarning("El modelo PaisDto no es válido.");
-                return BadRequest(ModelState);
-            }
-
-            var pais = _mapper.Map<Pais>(paisDto);
-            _paisRepository.Actualizar(pais);
-
-            if (await _paisRepository.GuardarCambios())
-            {
-                _logger.LogInformation($"País con ID {id} actualizado correctamente.");
-                return NoContent();
-            }
-
-            _logger.LogError("No se pudo actualizar el país.");
-            return BadRequest("No se pudo actualizar el país.");
+            _logger.LogWarning("El modelo PaisDto no es válido.");
+            return BadRequest(ModelState);
         }
 
-        // DELETE api/paises/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(int id)
+        var pais = _mapper.Map<Pais>(paisDto);
+        _paisRepository.Actualizar(pais);
+
+        if (await _paisRepository.GuardarCambios())
         {
-            _logger.LogInformation($"Eliminando país con ID {id}.");
-            var pais = await _paisRepository.ObtenerPorId(id);
-            if (pais == null)
-            {
-                _logger.LogWarning($"País con ID {id} no encontrado.");
-                return NotFound();
-            }
-
-            _paisRepository.Eliminar(pais);
-
-            if (await _paisRepository.GuardarCambios())
-            {
-                _logger.LogInformation($"País con ID {id} eliminado correctamente.");
-                return NoContent();
-            }
-
-            _logger.LogError($"Error al eliminar el país con ID {id}.");
-            return BadRequest("No se pudo eliminar el país.");
+            _logger.LogInformation($"País con ID {id} actualizado correctamente.");
+            return NoContent();
         }
+
+        _logger.LogError("No se pudo actualizar el país.");
+        return BadRequest("No se pudo actualizar el país.");
+    }
+
+    // DELETE api/paises/5
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        _logger.LogInformation($"Eliminando país con ID {id}.");
+        var pais = await _paisRepository.ObtenerPorId(id);
+        if (pais == null)
+        {
+            _logger.LogWarning($"País con ID {id} no encontrado.");
+            return NotFound();
+        }
+
+        _paisRepository.Eliminar(pais);
+
+        if (await _paisRepository.GuardarCambios())
+        {
+            _logger.LogInformation($"País con ID {id} eliminado correctamente.");
+            return NoContent();
+        }
+
+        _logger.LogError($"Error al eliminar el país con ID {id}.");
+        return BadRequest("No se pudo eliminar el país.");
     }
 }
--- backend-clientes/ApiBasesDeDatosSolucion/ApiBasesDeDatosProyecto/Helpers/PaisFaker.cs
+++ backend-clientes/ApiBasesDeDatosSolucion/ApiBasesDeDatosProyecto/Helpers/PaisFaker.cs
@@ -1,15 +1,14 @@
-﻿namespace ApiBasesDeDatosProyecto.Helpers
+﻿namespace ApiBasesDeDatosProyecto.Helpers;
+
+public class PaisFaker: Faker<Pais>
 {
-    public class PaisFaker: Faker<Pais>
+    private static int _idCounter = 1;
+    public PaisFaker() : base("es")
     {
-        private static int _idCounter = 1;
-        public PaisFaker() : base("es")
-        {
-            RuleFor(d => d.Id, f => _idCounter++);
-            RuleFor(d => d.Nombre, f => f.Address.Country());
-            RuleFor(d => d.Divisa, f => f.Finance.Currency().Code);
-            RuleFor(d => d.Iso3, f => f.Address.CountryCode());
-            RuleFor(d => d.Clientes, f => new ClienteFaker().Generate(5));
-        }
+        RuleFor(d => d.Id, f => _idCounter++);
+        RuleFor(d => d.Nombre, f => f.Address.Country());
+        RuleFor(d => d.Divisa, f => f.Finance.Currency().Code);
+        RuleFor(d => d.Iso3, f => f.PickRandom(ISO3166.Country.List.ToString()));
+        RuleFor(d => d.Clientes, f => new ClienteFaker().Generate(5));
     }
 }
