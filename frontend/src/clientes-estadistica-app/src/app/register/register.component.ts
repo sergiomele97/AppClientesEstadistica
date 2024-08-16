4ba5c27275d93e4a69ce410fdaf64c5967011c0d
Sergio Melero <sergiomele97@gmail.com>
2024-08-16 15:06:09 +0200

    Se implementa validación errores front AB#88

--- frontend/src/clientes-estadistica-app/src/app/register/register.component.ts
+++ frontend/src/clientes-estadistica-app/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../servicios/user.service';
+import { UsuarioAdmin } from '../interfaces/usuarioAdmin.interface';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  isFormBlocked = false;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder, private miServicio: UserService) { }
+
+
+  ngOnInit(): void {
+    // inyectamos el formulario
+    this.registerForm = this.fb.group({
+
+      // Se inicializan con valor vacio y validan con  validadores
+      // Todos required y alguno extra
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmpassword: ['', [Validators.required]],
+      phone: ['', Validators.required],
+    }, {
+      // Esto es un validador personalizado
+      validator: this.passwordMatchValidator('password', 'confirmpassword')
+    });
   }
 
+  // Método para comprobar que las dos contraseñas sean iguales
+  passwordMatchValidator(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+      const passwordControl = formGroup.get(password);
+      const confirmPasswordControl = formGroup.get(confirmPassword);
+
+      if (confirmPasswordControl?.errors && !confirmPasswordControl.errors['passwordMismatch']) {
+        return;
+      }
+
+      if (passwordControl?.value !== confirmPasswordControl?.value) {
+        confirmPasswordControl?.setErrors({ passwordMismatch: true });
+      } else {
+        confirmPasswordControl?.setErrors(null);
+      }
+    };
+  }
+  
+  // Metodo que se ejecuta al pulsar en el botón de registro
+  onSubmit(): void {
+
+    // 1. Es válido el formulario? (Se cumplen todos los validators?)
+    if (this.registerForm.invalid) {
+      this.checkFormErrors();
+      return; // Si no es válido, ahí acabamos
+    }
+
+    // 2. Coindicen las contraseñas?
+    if (this.registerForm.value.password !== this.registerForm.value.confirmpassword) {
+      alert('Las contraseñas no coinciden.');
+      return;
+    }
+
+    // 3. Crear objeto usuario
+    const usuario: User = {
+      email: this.registerForm.value.email,
+      password: this.registerForm.value.password,
+      confirmpassword: this.registerForm.value.confirmpassword,
+      phone: this.registerForm.value.phone,
+     
+    };
+
+    // 4. Registrar usuario en el backend
+    this.miServicio.registrarUsuario(usuario).subscribe(
+      response => {
+        console.log('Usuario registrado exitosamente', response);
+      },
+      error => {
+        if (error.status === 400) {
+          console.error('Error de validación', error.error);
+          alert('Error al registrar usuario: ' + error.error);
+        } else if (error.status === 0) {
+          console.error('No se pudo conectar al servidor.');
+          alert('No se pudo conectar al servidor.');
+        } else {
+          console.error(`Error ${error.status}: ${error.message}`);
+          alert('Error al registrar usuario: ' + error.message);
+        }
+      }
+    );
+  }
+
+  private checkFormErrors(): void {
+    this.isFormBlocked = this.registerForm.invalid;
+  }
+  
 }
