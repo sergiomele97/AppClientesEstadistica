ee6c20e8e17d17afd5847b2e7916829ee57bd9b4
Sergio Melero <sergiomele97@gmail.com>
2024-08-19 14:56:33 +0200

    Se añade enlace registro

--- frontend/src/clientes-estadistica-app/src/app/login/login.component.ts
+++ frontend/src/clientes-estadistica-app/src/app/login/login.component.ts
@@ -22,18 +22,13 @@ export class LoginComponent implements OnInit {
 
   constructor(private usuarioService: UserService, private route: Router) { }
 
-  ngOnInit() {
-    this.getUsuarios();
+  navigateToRegistro() {
+    this.route.navigate(['/registro']);
   }
 
-  getUsuarios() {
 
-    this.usuarioService.getUsuarios().subscribe( datos => {
-      this.usuarios = datos;
-
-      console.log(datos);
-    });
-
+  ngOnInit() {
+    
   }
 
   authentication() {
