a568f0601dba655e73a7df9d8176f91fe6119f0d
Iranzu <asanchezab3@educacion.navarra.es>
2024-08-20 13:06:13 +0200

    #106 vistas para visualizar tabla cluster desde back

---
+++ backend-estadistica/src/BackendEstadistica/Migrations/20240820095824_vistas.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendEstadistica.Migrations
+{
+    /// <inheritdoc />
+    public partial class vistas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql(@"
+            CREATE VIEW [dbo].[ClientesConBalance] AS
+            SELECT 
+                c.ClienteId,
+                c.Nombre,
+                c.Edad,
+                c.Sexo,
+                p.Nombre AS Pais,
+                (SELECT ISNULL(SUM(t.ImporteRecibido), 0) 
+                 FROM Transacciones t 
+                 WHERE t.ClienteDestinoId = c.ClienteId) 
+                 - 
+                (SELECT ISNULL(SUM(t.ImporteEnviado), 0) 
+                 FROM Transacciones t 
+                 WHERE t.ClienteOrigenId = c.ClienteId) 
+                AS Balance,
+                (SELECT COUNT(*) 
+                 FROM Transacciones t 
+                 WHERE t.ClienteOrigenId = c.ClienteId) 
+                AS NumeroGastos,
+                (SELECT COUNT(*) 
+                 FROM Transacciones t 
+                 WHERE t.ClienteDestinoId = c.ClienteId) 
+                AS NumeroIngresos
+            FROM Clientes c
+            INNER JOIN Pais p ON c.PaisId = p.PaisId
+        ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW [dbo].[ClientesConBalance]");
+        }
+    }
+}
