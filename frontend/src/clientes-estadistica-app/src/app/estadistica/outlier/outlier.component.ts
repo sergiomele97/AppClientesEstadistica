d1b5b4c381fa223a14ea00b871d5ab52dd3edf55
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-08-21 13:54:46 +0200

    AB#114 Se muestra un mensaje cuando no existen outliers

--- frontend/src/clientes-estadistica-app/src/app/estadistica/outlier/outlier.component.ts
+++ frontend/src/clientes-estadistica-app/src/app/estadistica/outlier/outlier.component.ts
@@ -9,16 +9,18 @@ import { TransaccionService } from 'src/app/servicios/transaccion.service';
 })
 export class OutlierComponent implements OnInit {
 
-  outliers: ITransaccion[];
+  outliers: ITransaccion[] = [];
+  vacio: boolean = false;
+  loading: boolean = true;
 
-  constructor( private transaccionService: TransaccionService ) { }
+  constructor(private transaccionService: TransaccionService) { }
 
   ngOnInit() {
-
-    this.transaccionService.obtenerOutlier().subscribe( datos => {
+    this.transaccionService.obtenerOutlier().subscribe(datos => {
       this.outliers = datos;
+      this.vacio = this.outliers.length === 0;
+      this.loading = false;
     });
-
   }
 
 }
