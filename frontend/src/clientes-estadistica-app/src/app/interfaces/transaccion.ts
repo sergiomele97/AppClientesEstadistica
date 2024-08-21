ae712e48299b6f2e99ec300ca53b696900427f15
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-08-21 10:45:37 +0200

    AB#97 Se conecta el back de outliers al front y se muestra los outlier y sus datos en la gráfica y la tabla

--- frontend/src/clientes-estadistica-app/src/app/interfaces/transaccion.ts
+++ frontend/src/clientes-estadistica-app/src/app/interfaces/transaccion.ts
@@ -5,6 +5,7 @@ export interface ITransaccion {
   importeRecibido: number;
   importeEnviado: number;
   fecha?: Date;
+  isOutlier: boolean;
   clienteOrigenId: number;
   clienteDestinoId: number;
 
