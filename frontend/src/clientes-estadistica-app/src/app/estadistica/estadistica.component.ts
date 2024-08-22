d501ca76f03fff4cd1c93869a2e5d7f49780671c
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-08-22 10:26:59 +0200

    AB#117 Recarga cada 30 segundos la sidebar para notificación outliers

--- frontend/src/clientes-estadistica-app/src/app/estadistica/estadistica.component.ts
+++ frontend/src/clientes-estadistica-app/src/app/estadistica/estadistica.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../clases/usuario';
 import { PruebaConexionService } from '../servicios/pruebaConexion.service';
 import { TransaccionService } from '../servicios/transaccion.service';
-import { ITransaccion } from '../interfaces/transaccion';
-import { FormaterFechaPipe } from '../pipes/formaterFecha.pipe';
+import { interval } from 'rxjs';
 
 // Decorador 
 @Component({
@@ -43,13 +42,21 @@ export class EstadisticaComponent implements OnInit {
       }
     );
 
-    this.transaccionService.obtenerOutlier().subscribe( datos => {
-      this.outliers = datos.length;
-    })
+    this.actualizarOutliers();
+
+    interval(30000).subscribe(() => {
+      this.actualizarOutliers()
+    });
 
   }
   // -------------------- Fin Método Sergio para Debuggear en Azure, no borrar:
   
+    actualizarOutliers() {
+      this.transaccionService.obtenerOutlier().subscribe( datos => {
+        this.outliers = datos.length;
+      });
+    }
+
   
 }
 
