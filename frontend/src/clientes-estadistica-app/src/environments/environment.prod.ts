4b777eff11addad54ae3a657123de20761d8d243
Sergio Melero <sergiomele97@gmail.com>
2024-08-26 09:23:40 +0200

    Dale python

--- frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
+++ frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     apiEstadisticas: 'https://backend-estadistica-g6afbvbagbe3d6gr.spaincentral-01.azurewebsites.net/api/estadisticas',
     apiUsuarios: 'https://backend-estadistica-g6afbvbagbe3d6gr.spaincentral-01.azurewebsites.net/api/usuarios',
-    apiClusters: 'https://backend-estadistica-g6afbvbagbe3d6gr.spaincentral-01.azurewebsites.net/cluster',
-    apiPrediccion: 'https://backend-estadistica-g6afbvbagbe3d6gr.spaincentral-01.azurewebsites.net/predict'
+    apiClusters: 'dataanalytics-azdxd7cmdbh0hmab.spaincentral-01.azurewebsites.net/cluster',
+    apiPrediccion: 'dataanalytics-azdxd7cmdbh0hmab.spaincentral-01.azurewebsites.net/predict'
   };
--- frontend/src/clientes-estadistica-app/src/environments/environment.ts
+++ frontend/src/clientes-estadistica-app/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     apiEstadisticas: 'https://localhost:7144/api/estadisticas',
     apiUsuarios: 'https://localhost:7144/api/usuarios',
-    apiClusters: 'http://127.0.0.1:5000/cluster',
+    apiClusters: 'http://localhost:5000/cluster',
     apiPrediccion: 'http://localhost:5000/predict'
   };
   
