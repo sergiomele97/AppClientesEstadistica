a63ac1e40d0b1ac4c20deb87a0753b88834746a6
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-08-28 12:31:53 +0200

    Error por mi culpa solucionado

--- backend-estadistica/src/BackendEstadistica/Controllers/EstadisticasController.cs
+++ backend-estadistica/src/BackendEstadistica/Controllers/EstadisticasController.cs
@@ -1,264 +1,265 @@
-﻿namespace BackendEstadistica.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-[Route("api/estadisticas")]
-[ApiController]
-public class EstadisticasController : ControllerBase
+namespace BackendEstadistica.Controllers
 {
-    private readonly ContextoBBDD _contextoBBDD;
-    private readonly IEstadisticasRepositorio _estadisticasRepositorio;
-    private readonly IMapper _mapper;
-
-    public EstadisticasController(
-        ContextoBBDD contextoBBDD,
-        IEstadisticasRepositorio estadisticasRepositorio,
-        IMapper mapper)
+    [Route("api/estadisticas")]
+    [ApiController]
+    public class EstadisticasController : ControllerBase
     {
-        _contextoBBDD = contextoBBDD;
-        _estadisticasRepositorio = estadisticasRepositorio;
-        _mapper = mapper;
-    }
+        private readonly ContextoBBDD _contextoBBDD;
+        private readonly IEstadisticasRepositorio _estadisticasRepositorio;
+        private readonly IMapper _mapper;
 
-    // Clientes
-    [HttpPost("crearCliente")]
-    public async Task<IActionResult> CrearCliente()
-    {
-        var paises = await _estadisticasRepositorio.GetPaisesAsync(); // Obtener todos los países existentes
-        var clienteFaker = new ClienteFaker(paises);
-        var clienteDto = clienteFaker.Generate();
-
-        var nuevoCliente = _mapper.Map<Cliente>(clienteDto);
-        await _estadisticasRepositorio.CrearClienteAsync(nuevoCliente);
-
-        return Ok("Cliente creado correctamente");
-    }
-
-    [HttpGet("getClientes")]
-    public async Task<IActionResult> GetClientes()
-    {
-        var clientes = await _estadisticasRepositorio.GetClientesAsync();
-        return Ok(_mapper.Map<List<Cliente>>(clientes));
-    }
-
-    [HttpGet("getCliente/{id}")]
-    public async Task<IActionResult> GetClienteById(int id)
-    {
-        var cliente = await _estadisticasRepositorio.GetClienteByIdAsync(id);
-
-        if (cliente == null)
+        public EstadisticasController(
+            ContextoBBDD contextoBBDD,
+            IEstadisticasRepositorio estadisticasRepositorio,
+            IMapper mapper)
         {
-            return NotFound("Cliente no encontrado.");
+            _contextoBBDD = contextoBBDD;
+            _estadisticasRepositorio = estadisticasRepositorio;
+            _mapper = mapper;
         }
 
-        return Ok(_mapper.Map<Cliente>(cliente));
-    }
-
-    // Transacciones
-    [HttpPost("crearTransaccion")]
-    public async Task<IActionResult> CrearTransaccion()
-    {
-        var clienteOrigen = await _estadisticasRepositorio.GetRandomClientAsync();
-        var clienteDestino = await _estadisticasRepositorio.GetRandomClientAsync();
-
-        var transaccionFaker = new TransaccionFaker(clienteOrigen, clienteDestino);
-        var transaccionDto = transaccionFaker.Generate();
-
-        var nuevaTransaccion = _mapper.Map<Transaccion>(transaccionDto);
-        await _estadisticasRepositorio.CrearTransaccionAsync(nuevaTransaccion);
-
-        // Detectar si esta transacción es un outlier
-        await _estadisticasRepositorio.DetectarOutliersAsync();
-
-        var transaccion = await _contextoBBDD.Transacciones
-            .Include(t => t.ClienteOrigen)
-            .FirstOrDefaultAsync(t => t.TransaccionId == nuevaTransaccion.TransaccionId);
-
-        if (transaccion != null && transaccion.IsOutlier == true)
+        // Clientes
+        [HttpPost("crearCliente")]
+        public async Task<IActionResult> CrearCliente()
         {
-            return Ok(new
+            var paises = await _estadisticasRepositorio.GetPaisesAsync(); // Obtener todos los países existentes
+            var clienteFaker = new ClienteFaker(paises);
+            var clienteDto = clienteFaker.Generate();
+
+            var nuevoCliente = _mapper.Map<Cliente>(clienteDto);
+            await _estadisticasRepositorio.CrearClienteAsync(nuevoCliente);
+
+            return Ok("Cliente creado correctamente");
+        }
+
+        [HttpGet("getClientes")]
+        public async Task<IActionResult> GetClientes()
+        {
+            var clientes = await _estadisticasRepositorio.GetClientesAsync();
+            return Ok(_mapper.Map<List<Cliente>>(clientes));
+        }
+
+        [HttpGet("getCliente/{id}")]
+        public async Task<IActionResult> GetClienteById(int id)
+        {
+            var cliente = await _estadisticasRepositorio.GetClienteByIdAsync(id);
+
+            if (cliente == null)
             {
-                Message = "Outlier detectado",
-                Cliente = transaccion.ClienteOrigen.Nombre,
-                ImporteEnviado = transaccion.ImporteEnviado
-            });
+                return NotFound("Cliente no encontrado.");
+            }
+
+            return Ok(_mapper.Map<Cliente>(cliente));
         }
 
-        return Ok("Transacción creada correctamente");
-    }
-
-    [HttpGet("getTransacciones")]
-    public async Task<IActionResult> GetTransacciones()
-    {
-        var transacciones = await _estadisticasRepositorio.GetTransaccionesAsync();
-        return Ok(_mapper.Map<List<Transaccion>>(transacciones));
-    }
-
-    [HttpGet("getTransacciones/{id}")]
-    public async Task<IActionResult> GetTransaccionesById(int id)
-    {
-        var transaccion = await _estadisticasRepositorio.GetTransaccionByIdAsync(id);
-
-        if (transaccion == null)
+        // Transacciones
+        [HttpPost("crearTransaccion")]
+        public async Task<IActionResult> CrearTransaccion()
         {
-            return NotFound("Transacción no encontrada.");
+            var clienteOrigen = await _estadisticasRepositorio.GetRandomClientAsync();
+            var clienteDestino = await _estadisticasRepositorio.GetRandomClientAsync();
+
+            var transaccionFaker = new TransaccionFaker(clienteOrigen, clienteDestino);
+            var transaccionDto = transaccionFaker.Generate();
+
+            var nuevaTransaccion = _mapper.Map<Transaccion>(transaccionDto);
+            await _estadisticasRepositorio.CrearTransaccionAsync(nuevaTransaccion);
+
+            // Detectar si esta transacción es un outlier
+            await _estadisticasRepositorio.DetectarOutliersAsync();
+
+            var transaccion = await _contextoBBDD.Transacciones
+                .Include(t => t.ClienteOrigen)
+                .FirstOrDefaultAsync(t => t.TransaccionId == nuevaTransaccion.TransaccionId);
+
+            if (transaccion != null && transaccion.IsOutlier == true)
+            {
+                return Ok(new
+                {
+                    Message = "Outlier detectado",
+                    Cliente = transaccion.ClienteOrigen.Nombre,
+                    ImporteEnviado = transaccion.ImporteEnviado
+                });
+            }
+
+            return Ok("Transacción creada correctamente");
         }
 
-        return Ok(_mapper.Map<Transaccion>(transaccion));
-    }
-
-    // Divisas
-    [HttpPost("crearDivisas")]
-    public async Task<IActionResult> CrearDivisas()
-    {
-        // Lista de nombres de divisas
-        var divisas = new List<string>
+        [HttpGet("getTransacciones")]
+        public async Task<IActionResult> GetTransacciones()
         {
-            "AFN", "ALL", "EUR", "AOA", "XCD", "SAR", "DZD", "ARS", "AMD", "AUD", "AZN", "BSD", "BHD",
-            "BDT", "BBD", "BZD", "XOF", "BYN", "MMK", "BOB", "BAM", "BWP", "BRL", "BND", "BGN", "BIF",
-            "INR", "CVE", "KHR", "XAF", "CAD", "QAR", "CLP", "CNY", "COP", "KMF", "KPW", "KRW", "CRC",
-            "HRK", "CUP", "CZK", "DKK", "EGP", "USD", "AED", "ERN", "GBP", "SZL", "GTQ", "GNF", "GYD",
-            "HTG", "HNL", "HUF", "IDR", "IRR", "IQD", "ISK", "JMD", "JPY", "JOD", "KZT", "KES", "KGS",
-            "KWD", "LAK", "LVL", "LBP", "LRD", "LYD", "CHF", "MGA", "MYR", "MWK", "MVR", "MDL", "MNT",
-            "MAD", "MUR", "MRU", "MXN", "NAD", "NPR", "NIO", "NGN", "NOK", "NZD", "OMR", "PKR", "PAB",
-            "PGK", "PYG", "PEN", "PLN", "RON", "RUB", "RSD", "SCR", "SLL", "SGD", "SYP", "SOS", "LKR",
-            "SDG", "SEK", "STN", "RWF"
-        };
-
-        var fecha = DateTime.Now;
-
-        foreach (var divisa in divisas)
-        {
-            var divisaFaker = new DivisaFaker(divisa, fecha);
-            var divisaDto = divisaFaker.Generate();
-            var nuevaDivisa = _mapper.Map<Divisa>(divisaDto);
-            await _estadisticasRepositorio.CrearDivisaAsync(nuevaDivisa);
+            var transacciones = await _estadisticasRepositorio.GetTransaccionesAsync();
+            return Ok(_mapper.Map<List<Transaccion>>(transacciones));
         }
 
-        return Ok("Divisas creadas correctamente");
-    }
-
-    [HttpGet("getDivisa/{id}")]
-    public async Task<IActionResult> GetDivisaById(int id)
-    {
-        var divisa = await _estadisticasRepositorio.GetDivisaByIdAsync(id);
-
-        if (divisa == null)
+        [HttpGet("getTransacciones/{id}")]
+        public async Task<IActionResult> GetTransaccionesById(int id)
         {
-            return NotFound("Divisa no encontrada.");
+            var transaccion = await _estadisticasRepositorio.GetTransaccionByIdAsync(id);
+
+            if (transaccion == null)
+            {
+                return NotFound("Transacción no encontrada.");
+            }
+
+            return Ok(_mapper.Map<Transaccion>(transaccion));
         }
 
-        return Ok(_mapper.Map<Divisa>(divisa));
-    }
-
-    [HttpGet("getDivisas")]
-    public async Task<IActionResult> GetDivisas()
-    {
-        var divisas = await _estadisticasRepositorio.GetDivisasAsync();
-        return Ok(_mapper.Map<List<Divisa>>(divisas));
-    }
-
-    [HttpGet("outliers")]
-    public async Task<IActionResult> ObtenerTransaccionesOutliers()
-    {
-        var transaccionesOutliers = await _contextoBBDD.Transacciones
-            .Include(t => t.ClienteOrigen)
-            .Include(t => t.ClienteDestino)
-            .Where(t => t.IsOutlier == true)
-            .ToListAsync();
-
-        return Ok(transaccionesOutliers);
-    }
-
-    [HttpGet("ultimas-transacciones/{clienteId}")]
-    public async Task<IActionResult> ObtenerUltimasTransacciones(int clienteId)
-    {
-        var transaccionesMayores = await _contextoBBDD.Transacciones
-            .Include(t => t.ClienteOrigen)
-            .Include(t => t.ClienteDestino)
-            .Where(t => t.ClienteOrigenId == clienteId)
-            .OrderByDescending(t => t.ImporteEnviado)
-            .Take(5)
-            .ToListAsync();
-
-        return Ok(transaccionesMayores);
-    }
-
-    [HttpGet("outliers-vistos")]
-    public async Task<IActionResult> ObtenerOutliersVistos()
-    {
-        var outliersVistos = await _contextoBBDD.Transacciones
-            .Include(t => t.ClienteOrigen)
-            .Include(t => t.ClienteDestino)
-            .Where(t => t.IsOutlierVisto == true)
-            .ToListAsync();
-
-        return Ok(outliersVistos);
-    }
-
-    [HttpPut("resolucionOutlier/{idTransaccion}")]
-    public async Task<IActionResult> EliminarOutlier([FromRoute] int idTransaccion)
-    {
-        var resultado = await _estadisticasRepositorio.EliminarOutlierAsync(idTransaccion);
-
-        if (!resultado)
+        // Divisas
+        [HttpPost("crearDivisas")]
+        public async Task<IActionResult> CrearDivisas()
         {
-            return NotFound("Transacción no encontrada o no es un outlier.");
+            // Lista de nombres de divisas
+            var divisas = new List<string>
+            {
+                "AFN", "ALL", "EUR", "AOA", "XCD", "SAR", "DZD", "ARS", "AMD", "AUD", "AZN", "BSD", "BHD",
+                "BDT", "BBD", "BZD", "XOF", "BYN", "MMK", "BOB", "BAM", "BWP", "BRL", "BND", "BGN", "BIF",
+                "INR", "CVE", "KHR", "XAF", "CAD", "QAR", "CLP", "CNY", "COP", "KMF", "KPW", "KRW", "CRC",
+                "HRK", "CUP", "CZK", "DKK", "EGP", "USD", "AED", "ERN", "GBP", "SZL", "GTQ", "GNF", "GYD",
+                "HTG", "HNL", "HUF", "IDR", "IRR", "IQD", "ISK", "JMD", "JPY", "JOD", "KZT", "KES", "KGS",
+                "KWD", "LAK", "LVL", "LBP", "LRD", "LYD", "CHF", "MGA", "MYR", "MWK", "MVR", "MDL", "MNT",
+                "MAD", "MUR", "MRU", "MXN", "NAD", "NPR", "NIO", "NGN", "NOK", "NZD", "OMR", "PKR", "PAB",
+                "PGK", "PYG", "PEN", "PLN", "RON", "RUB", "RSD", "SCR", "SLL", "SGD", "SYP", "SOS", "LKR",
+                "SDG", "SEK", "STN", "RWF"
+            };
+
+            var fecha = DateTime.Now;
+
+            foreach (var divisa in divisas)
+            {
+                var divisaFaker = new DivisaFaker(divisa, fecha);
+                var divisaDto = divisaFaker.Generate();
+                var nuevaDivisa = _mapper.Map<Divisa>(divisaDto);
+                await _estadisticasRepositorio.CrearDivisaAsync(nuevaDivisa);
+            }
+
+            return Ok("Divisa creada correctamente");
         }
 
-        return Ok("El outlier se eliminó con éxito.");
-    }
-
-    // Conversiones
-    [HttpPost("crearConversion")]
-    public async Task<IActionResult> CrearConversion()
-    {
-        var cliente = await _estadisticasRepositorio.GetRandomClientAsync();
-        var conversionFaker = new ConversionFaker(cliente);
-        var conversionDto = conversionFaker.Generate();
-
-        var nuevaConversion = _mapper.Map<Conversion>(conversionDto);
-        await _estadisticasRepositorio.CrearConversionAsync(nuevaConversion);
-
-        return Ok("Conversión creada correctamente");
-    }
-
-    [HttpGet("getConversiones")]
-    public async Task<IActionResult> GetConversiones()
-    {
-        var conversiones = await _estadisticasRepositorio.GetConversionesAsync();
-        return Ok(_mapper.Map<List<Conversion>>(conversiones));
-    }
-
-    [HttpGet("getConversion/{id}")]
-    public async Task<IActionResult> GetConversionById(int id)
-    {
-        var conversion = await _estadisticasRepositorio.GetConversionByIdAsync(id);
-
-        if (conversion == null)
+        [HttpGet("getDivisa/{nombre}")]
+        public async Task<IActionResult> GetDivisaByName(string nombre)
         {
-            return NotFound("Conversión no encontrada.");
+            var divisaNombre = await _estadisticasRepositorio.GetDivisaByNameAsync(nombre);
+            return Ok(_mapper.Map<List<Divisa>>(divisaNombre));
         }
 
-        return Ok(_mapper.Map<Conversion>(conversion));
-    }
-
-    // Paises
-    [HttpGet("getPaises")]
-    public async Task<IActionResult> GetPaises()
-    {
-        var paises = await _estadisticasRepositorio.GetPaisesAsync();
-        return Ok(_mapper.Map<List<Pais>>(paises));
-    }
-
-    [HttpGet("getPaises/{id}")]
-    public async Task<IActionResult> GetPaisById(int id)
-    {
-        var pais = await _estadisticasRepositorio.GetPaisByIdAsync(id);
-
-        if (pais == null)
+        [HttpGet("getDivisas")]
+        public async Task<IActionResult> GetDivisas()
         {
-            return NotFound("País no encontrado.");
+            var divisas = await _estadisticasRepositorio.GetDivisasAsync();
+            return Ok(_mapper.Map<List<Divisa>>(divisas));
         }
 
-        return Ok(_mapper.Map<Pais>(pais));
+        [HttpGet("outliers")]
+        public async Task<IActionResult> ObtenerTransaccionesOutliers()
+        {
+            var transaccionesOutliers = await _contextoBBDD.Transacciones
+                .Include(t => t.ClienteOrigen)
+                .Include(t => t.ClienteDestino)
+                .Where(t => t.IsOutlier == true)
+                .ToListAsync();
+
+            return Ok(transaccionesOutliers);
+        }
+
+        [HttpGet("ultimas-transacciones/{clienteId}")]
+        public async Task<IActionResult> ObtenerUltimasTransacciones(int clienteId)
+        {
+            var transaccionesMayores = await _contextoBBDD.Transacciones
+                .Include(t => t.ClienteOrigen)
+                .Include(t => t.ClienteDestino)
+                .Where(t => t.ClienteOrigenId == clienteId)
+                .OrderByDescending(t => t.ImporteEnviado)
+                .Take(5)
+                .ToListAsync();
+
+            return Ok(transaccionesMayores);
+        }
+
+        [HttpGet("outliers-vistos")]
+        public async Task<IActionResult> ObtenerOutliersVistos()
+        {
+            var outliersVistos = await _contextoBBDD.Transacciones
+                .Include(t => t.ClienteOrigen)
+                .Include(t => t.ClienteDestino)
+                .Where(t => t.IsOutlierVisto == true)
+                .ToListAsync();
+
+            return Ok(outliersVistos);
+        }
+
+        [HttpPut("resolucionOutlier/{idTransaccion}")]
+        public async Task<IActionResult> EliminarOutlier([FromRoute] int idTransaccion)
+        {
+            var resultado = await _estadisticasRepositorio.EliminarOutlierAsync(idTransaccion);
+
+            if (!resultado)
+            {
+                return NotFound("Transacción no encontrada o no es un outlier.");
+            }
+
+            return Ok("El outlier se eliminó con éxito.");
+        }
+
+        // Conversiones
+        [HttpPost("crearConversion")]
+        public async Task<IActionResult> CrearConversion()
+        {
+            var cliente = await _estadisticasRepositorio.GetRandomClientAsync();
+            var conversionFaker = new ConversionFaker(cliente);
+            var conversionDto = conversionFaker.Generate();
+
+            var nuevaConversion = _mapper.Map<Conversion>(conversionDto);
+            await _estadisticasRepositorio.CrearConversionAsync(nuevaConversion);
+
+            return Ok("Conversión creada correctamente");
+        }
+
+        [HttpGet("getConversiones")]
+        public async Task<IActionResult> GetConversiones()
+        {
+            var conversiones = await _estadisticasRepositorio.GetConversionesAsync();
+            return Ok(_mapper.Map<List<Conversion>>(conversiones));
+        }
+
+        [HttpGet("getConversion/{id}")]
+        public async Task<IActionResult> GetConversionById(int id)
+        {
+            var conversion = await _estadisticasRepositorio.GetConversionByIdAsync(id);
+
+            if (conversion == null)
+            {
+                return NotFound("Conversión no encontrada.");
+            }
+
+            return Ok(_mapper.Map<Conversion>(conversion));
+        }
+
+        // Paises
+        [HttpGet("getPaises")]
+        public async Task<IActionResult> GetPaises()
+        {
+            var paises = await _estadisticasRepositorio.GetPaisesAsync();
+            return Ok(_mapper.Map<List<Pais>>(paises));
+        }
+
+        [HttpGet("getPaises/{id}")]
+        public async Task<IActionResult> GetPaisById(int id)
+        {
+            var pais = await _estadisticasRepositorio.GetPaisByIdAsync(id);
+
+            if (pais == null)
+            {
+                return NotFound("País no encontrado.");
+            }
+
+            return Ok(_mapper.Map<Pais>(pais));
+        }
     }
 }
