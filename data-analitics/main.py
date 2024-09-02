4c0a2e669aa16620671c5e16f80bad9f01d59247
Iranzu <asanchezab3@educacion.navarra.es>
2024-09-02 09:25:57 +0200

    #106 vista hecha y con tabla visualizada

--- data-analitics/main.py
+++ data-analitics/main.py
@@ -30,7 +30,7 @@ def cluster():
 
     recibido = request.json
     data = recibido['data']
-    #print("data cluster: ",data, flush=True)
+    print("data cluster: ",data, flush=True)
     n_clusters = recibido['nCluster']
     kmeans = KMeans(n_clusters=n_clusters, random_state=0)
     kmeans.fit(data)
--- frontend/src/clientes-estadistica-app/src/app/estadistica/clusters/clusters.component.ts
+++ frontend/src/clientes-estadistica-app/src/app/estadistica/clusters/clusters.component.ts
@@ -56,7 +56,7 @@ export class ClustersComponent implements OnInit, OnDestroy {
     // Recorrer todos los clientes y extraer los datos necesarios junto con el balance calculado
     this.datos = this.clientesBalance.map(cliente => {
       const sexo = cliente.sexo === 'Masculino' ? 0 : 1;
-      return [cliente.edad, cliente.balance, sexo, cliente.numeroGastos, cliente.numeroIngresos];
+      return [cliente.clienteId, cliente.edad, sexo, cliente.pais, cliente.balance, cliente.numeroGastos, cliente.numeroIngresos];
     });
     console.log("Datos procesados para enviar al backend:", this.datos);
   }
@@ -64,15 +64,26 @@ export class ClustersComponent implements OnInit, OnDestroy {
   private async enviarDatosBackend(datos: any[], nCluster: number): Promise<void> {
     try {
       console.log("Enviando datos al backend Python:", datos, "Número de clusters:", nCluster);
-      const response = await this.http.post<any>(this.apiUrl, { data: datos, nCluster: nCluster }).toPromise();
+      const datos_cluster = datos.map(individuo => [individuo[1],individuo[2], individuo[4],individuo[5],individuo[6]]); 
+      //console.log("Datos cluster:", datos_cluster);
+      const response = await this.http.post<any>(this.apiUrl, { data: datos_cluster, nCluster: nCluster }).toPromise();
       console.log("Respuesta del backend Python:", response);
 
       const etiqueta = response.etiqueta || [];
       this.daviesBouldinIndex = response.db || 0;
       this.dataService.setLabel(etiqueta);
 
-      const datosReducidos = datos.map(individuo => individuo.slice(0, 2)); // solo se muestran dos variables
+      const datosReducidos = datos.map(individuo => [individuo[1], individuo[4]]); // solo se muestran dos variables
       this.dataService.setSelectedDataCluster(datosReducidos); // mandamos los datos cortados a la global para visualizar en cluster
+      datos = datos.map(individuo => [
+        individuo[0], // clienteId
+        individuo[1], // edad
+        individuo[2] === 0 ? "Masculino" : "Femenino", // sexo
+        individuo[3], // pais
+        individuo[4], // balance
+        individuo[5], // numeroGastos
+        individuo[6]  // numeroIngresos
+      ]);
       this.dataService.setSelectedDataTable(datos); // mandamos los datos completos a la global para visualizar en la tabla
 
       // Combinar los datos con las etiquetas
