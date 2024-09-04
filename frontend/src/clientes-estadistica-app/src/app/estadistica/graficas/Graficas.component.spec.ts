2468c556a94b9e200c16312b59cb52e64850ed6f
Sergio Melero <sergiomele97@gmail.com>
2024-09-04 13:31:24 +0200

    Mayusculas mal

--- frontend/src/clientes-estadistica-app/src/app/estadistica/graficas/Graficas.component.spec.ts
+++ frontend/src/clientes-estadistica-app/src/app/estadistica/graficas/Graficas.component.spec.ts
@@ -3,9 +3,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { GraficasComponent } from './graficas.component';
+import { GraficasComponent } from './Graficas.component';
 
-describe('graficasComponent', () => {
+describe('GraficasComponent', () => {
   let component: GraficasComponent;
   let fixture: ComponentFixture<GraficasComponent>;
 
