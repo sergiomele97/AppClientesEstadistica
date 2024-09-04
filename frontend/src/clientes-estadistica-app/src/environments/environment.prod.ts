c17ae63852dc7a6cf966b82a90af3616ff502a1c
Sergio Melero <sergiomele97@gmail.com>
2024-09-04 13:03:29 +0200

    Comentarios mal

--- frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
+++ frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
     production: true,
     apiEstadisticas: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/estadisticas',
     apiUsuarios: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/usuarios',
-    // Signal R
+    
     apiNotification: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/notificationHub',
 
-    // Backend python
+    
     apiClusters: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/cluster',
     apiPrediccion: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/predict',
 
