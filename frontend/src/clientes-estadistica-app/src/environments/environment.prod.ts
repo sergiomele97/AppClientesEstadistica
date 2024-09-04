5be706914e6ebbb74dc33c90f90d5f7bf0c7abb6
Sergio Melero <sergiomele97@gmail.com>
2024-09-04 13:10:21 +0200

    Comas mal

--- frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
+++ frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     apiEstadisticas: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/estadisticas',
     apiUsuarios: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/usuarios',
-    apiNotification: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/notificationHub',
+    //apiNotification: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/notificationHub',
     apiClusters: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/cluster',
     apiPrediccion: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/predict',
     apiProcedure: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/api/TransaccionesProceso'
