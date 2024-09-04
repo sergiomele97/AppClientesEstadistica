98d737caac19b9664d140e60460a2eaabf2ff14a
Sergio Melero <sergiomele97@gmail.com>
2024-09-04 10:23:56 +0200

    Front: Referencias a produccion actualizadas

--- frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
+++ frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiEstadisticas: 'https://backend-estadistica-g6afbvbagbe3d6gr.spaincentral-01.azurewebsites.net/api/estadisticas',
-    apiUsuarios: 'https://backend-estadistica-g6afbvbagbe3d6gr.spaincentral-01.azurewebsites.net/api/usuarios',
-    apiClusters: 'https://datapython-ene3fsaxg4hne9cn.spaincentral-01.azurewebsites.net/cluster',
-    apiPrediccion: 'https://datapython-ene3fsaxg4hne9cn.spaincentral-01.azurewebsites.net/predict'
+    apiEstadisticas: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/estadisticas',
+    apiUsuarios: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/usuarios',
+    apiClusters: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/cluster',
+    apiPrediccion: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/predict'
   };
