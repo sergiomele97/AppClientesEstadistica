54e5f9662009f544dc1c4cadbda2179e32ab21d2
Sergio Melero <sergiomele97@gmail.com>
2024-09-04 10:49:20 +0200

    Ajuste variable entorno signal R en front

--- frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
+++ frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
@@ -2,6 +2,11 @@ export const environment = {
     production: true,
     apiEstadisticas: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/estadisticas',
     apiUsuarios: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/usuarios',
+    // Signal R
+    apiNotification: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/notificationHub',
+
+    // Backend python
     apiClusters: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/cluster',
-    apiPrediccion: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/predict'
+    apiPrediccion: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/predict',
+
   };
