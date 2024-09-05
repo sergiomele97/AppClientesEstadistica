431a935233602fb0641ad5b45eee469422e61ca0
CarlosRyu98 <ccoderyu@gmail.com>
2024-09-05 13:01:52 +0200

    amaja está loca 2

--- backend-estadistica/src/BackendEstadistica/Program.cs
+++ backend-estadistica/src/BackendEstadistica/Program.cs
@@ -97,6 +97,7 @@ public class Program
             {
                 builder.AllowAnyOrigin()
                 .AllowAnyMethod()
+                .AllowCredentials()
                 .AllowAnyHeader();
             });
         });
