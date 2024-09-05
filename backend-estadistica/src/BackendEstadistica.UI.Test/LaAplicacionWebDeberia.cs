5134e79edafed503fc57dc6b77fed24d757e3164
Sergio Melero <sergiomele97@gmail.com>
2024-09-05 15:05:44 +0200

    Pruebas web

--- backend-estadistica/src/BackendEstadistica.UI.Test/LaAplicacionWebDeberia.cs
+++ backend-estadistica/src/BackendEstadistica.UI.Test/LaAplicacionWebDeberia.cs
@@ -1,14 +1,14 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace BackendEstadistica.UI.Test
 {
     public class LaAplicacionWebDeberia
     {
-
         private readonly IWebDriver _driver;
 
-        public LaAplicacionWebDeberia(IWebDriver driver)
+        public LaAplicacionWebDeberia()
         {
             _driver = new ChromeDriver();
         }
@@ -16,12 +16,16 @@ namespace BackendEstadistica.UI.Test
         [Fact]
         public void MostrarBienvenida()
         {
-
             _driver.Navigate().GoToUrl("https://localhost:4200/");
-            Assert.Equal("ClientesEstadisticaApp", _driver.Title);
-         
-            _driver.Quit();
 
+            // Esperar hasta 15 segundos para que el título de la página sea el esperado
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(15));
+            wait.Until(driver => driver.Title == "ClientesEstadisticaApp");
+
+            // Verificar que el título es el esperado
+            Assert.Equal("ClientesEstadisticaApp", _driver.Title);
+
+            _driver.Quit();
         }
     }
 }
