88cd7b251f8d0e90f3d950db0656e96f6d5f6f90
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-09-05 10:09:22 +0200

    sin coomments

--- frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
+++ frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
@@ -3,14 +3,11 @@ export const environment = {
     apiEstadisticas: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/estadisticas',
     apiUsuarios: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/usuarios',
 
-    //SignalR
     apiNotification: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/notificationHub',
 
-    //Python
     apiClusters: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/cluster',
     apiPrediccion: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/predict',
 
-    //Vistas y Procesos
     apiBalance: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/ClientesConBalance',
     apiProcedure: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/TransaccionesProceso'
 
