e12f2a30374e67d1ea5d266c66a7cc8b45f96504
rlopezbarbarin <robertolopezb2003@hotmail.com>
2024-09-05 10:03:26 +0200

    Urls

--- frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
+++ frontend/src/clientes-estadistica-app/src/environments/environment.prod.ts
@@ -2,7 +2,11 @@ export const environment = {
     production: true,
     apiEstadisticas: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/estadisticas',
     apiUsuarios: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/api/usuarios',
+
+    //SignalR
     apiNotification: 'https://chachibackend-cudsb0anfdcncddp.spaincentral-01.azurewebsites.net/notificationHub',
+
+    //Python
     apiClusters: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/cluster',
     apiPrediccion: 'https://chachidatos-hcegapfwcua8cgg3.spaincentral-01.azurewebsites.net/predict',
 
